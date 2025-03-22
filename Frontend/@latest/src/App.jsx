f643632d5ba9b456c30ddb5faf4e23f3ee73ecbd
SimranRojia <149954788+SimranRojia@users.noreply.github.com>
2025-03-22 13:06:15 +0530

    about

--- Frontend/@latest/src/App.jsx
+++ Frontend/@latest/src/App.jsx
@@ -8,6 +8,7 @@ import Navbar from './Navbar'
 import Payment from './Screens/Payment'
 import Transaction from './Screens/Transaction'
 import Dashboard from './Dashboard'
+import About from "./Screens/About"
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path="/" element={<Start/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/signup" element={<SignUp/>} />
+      <Route path="/about" element={<About/>} />
       
     </Routes>
     <Navbar/>
